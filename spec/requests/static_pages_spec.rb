6fe4119ae55b86a3103565b066bfba52d13a6b71
samuelralak <samuelralak@ovi.com>
2014-06-06 17:42:26 +0300

    added tests for title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Social Network')
 		end
+
+		it "Should have title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title('Social Network | Home')
+		end
 	end
 
 	describe "Help page" do
@@ -14,12 +19,22 @@ describe "StaticPages" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+		it "Should have title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title('Social Network | Help')
+		end
 	end
 
 	describe "About page" do
 		it "Should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
+		end
+
+		it "Should have title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title('Social Network | About Us')
 		end
 	end
 
