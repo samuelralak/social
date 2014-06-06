11495284ccd79283ac243d23717084fe6f2d53ec
samuelralak <samuelralak@ovi.com>
2014-06-06 16:39:24 +0300

    initial test code for the help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,12 @@ describe "StaticPages" do
 			expect(page).to have_content('Social Network')
 		end
 	end
-	
+
+	describe "Help page" do
+		it "Should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+	end
+
 end
