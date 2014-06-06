4b6a0cce42807a60d3f175376344927d9d15f9b1
samuelralak <samuelralak@ovi.com>
2014-06-06 18:53:55 +0300

    added tests for the contact page #1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,16 @@ describe "StaticPages" do
 		end
 	end
 
+	describe "Contact page" do
+		it "Should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact Us')
+		end
+
+		it "Should have title 'Contact Us'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title('Social Network | Contact Us')
+		end
+	end
+
 end
