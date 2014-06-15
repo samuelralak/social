9c65d99d9b92333024b5320af08d479365cb9f28
samuelralak <samuelralak@ovi.com>
2014-06-15 21:46:36 +0300

    now using named routes #2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,53 +4,53 @@ describe "StaticPages" do
 
 	describe "Home page" do
 		it "Should have the content 'Social Network'" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_content('Social Network')
 		end
 
 		it "Should have base title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_title('Social Network')
 		end
 
 		it "Should have custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).not_to have_title('| Home')
 		end
 	end
 
 	describe "Help page" do
 		it "Should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 
 		it "Should have title 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_title('Social Network | Help')
 		end
 	end
 
 	describe "About page" do
 		it "Should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 
 		it "Should have title 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_title('Social Network | About Us')
 		end
 	end
 
 	describe "Contact page" do
 		it "Should have the content 'Contact Us'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_content('Contact Us')
 		end
 
 		it "Should have title 'Contact Us'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_title('Social Network | Contact Us')
 		end
 	end
