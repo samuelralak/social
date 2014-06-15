46bcaea369bb539c9bad64e0792a1896d2e36624
samuelralak <samuelralak@ovi.com>
2014-06-15 23:29:26 +0300

    fixes #2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,64 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static Pages" do
 
 	subject { page }
 
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+	end
+
 	describe "Home page" do
 		before { visit root_path }
+		let(:heading) 	 {'Social Network'}
+		let(:page_title) {''}
 
-		it { should have_content('Social Network') }
-		it { should have_title(full_title('')) } 
+		it_should_behave_like "all static pages" 
 		it { should_not have_title('| Home') }
 	end
 
 	describe "Help page" do
 		before { visit help_path }
+		let(:heading) 	 {'Help'}
+		let(:page_title) {'Help'}
 
-		it { should have_content('Help') }
-		it { should have_title(full_title('Help')) }
+		it_should_behave_like "all static pages" 
 	end
 
 	describe "About page" do
 		before { visit about_path }
+		let(:heading) 	 {'About Us'}
+		let(:page_title) {'About Us'}
 
-		it { should have_content('About Us') }
-		it { should have_title(full_title('About Us')) }
+		it_should_behave_like "all static pages" 
 	end
 
 	describe "Contact page" do
 		before { visit contact_path }
+		let(:heading) 	 {'Contact Us'}
+		let(:page_title) {'Contact Us'}
 
-		it { should have_content('Contact Us') }
-		it { should have_title(full_title('Contact Us')) }
+		it_should_behave_like "all static pages" 
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+
+		click_link "Sign Up now!"
+		expect(page).to have_title(full_title('Sign Up'))
+
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact Us'))
+
+		click_link "SOCIAL NETWORK"
+		expect(page).to have_title(full_title(''))
 	end
 
 end
