54c177b31f168dd708414845cd6fff556e4e68b9
samuelralak <samuelralak@ovi.com>
2014-06-16 03:38:35 +0300

    added tests for password presence and mismatch #3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Example User", email: "user@example.com") }
+	before { @user = User.new(name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password") }
 
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 
 	it { should be_valid }
 
@@ -53,6 +55,19 @@ describe User do
 			user_with_same_email.save
 		end
 
+		it { should_not be_valid }
+	end
+
+	describe "when password is not present" do
+		before do
+			@user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
+		end
+
+		it { should_not be_valid }
+	end
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "missmatch"}
 		it { should_not be_valid }
 	end
 end
