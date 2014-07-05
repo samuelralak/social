eb9bbfa3bfe9b95443824051b62a469a064a5081
samuelralak <samuelralak@ovi.com>
2014-07-05 23:48:32 +0300

    rescue NoMethodError temporarily

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+  	begin
+  		@user = User.find(params[:id])
+
+  	rescue ActiveRecord::RecordNotFound
+
+  		flash[:notice] = "Could not find user"
+  		
+  	end
+  end
 end
