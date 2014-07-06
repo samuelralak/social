bbafa209450b2104a74d9d654369b736a8d0b535
samuelralak <samuelralak@ovi.com>
2014-07-07 00:58:46 +0300

    tests for flash persistence bug after authentication failure

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,6 +18,12 @@ describe "Authentication" do
 
 			it { should have_title('Sign in') }
       		it { should have_selector('div.alert.alert-danger') }
+
+      		describe "after visiting another page" do
+      			before { click_link "Home" }
+
+				it { should have_selector('div.alert.alert-danger') }      			
+      		end
 		end
 
 		describe "with valid information" do
