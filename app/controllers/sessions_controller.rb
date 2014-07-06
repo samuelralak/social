99a0f256d1714c09b00014154f029a24ce9f2030
samuelralak <samuelralak@ovi.com>
2014-07-07 01:05:34 +0300

    complete sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,10 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 
 		if user && user.authenticate(params[:session][:password])
-			# Sign in user and redirect to the user's show page
-
+			sign_in user
+			redirect_to user
 		else
-			flash.now[:danger] = "Invalid email/password combination" # Not quite right
+			flash.now[:danger] = "Invalid email/password combination"
 			render 'new'
 		end
 	end
