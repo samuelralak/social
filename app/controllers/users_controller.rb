a4f6287d53d4f4116bbc56114111d42a2fac5c80
samuelralak <samuelralak@ovi.com>
2014-07-07 02:46:13 +0300

    signing in the user upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if @user.save
+      sign_in @user
   		flash[:success] = "Welcome to the Social Network"
   		redirect_to @user
   	else
