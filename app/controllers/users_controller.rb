d7cfb7a73277fda1caf85c70ef2899612e235d3b
samuelralak <samuelralak@ovi.com>
2014-07-06 12:56:25 +0300

    code to handle successful user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if @user.save
-  		# Handle a successful save.
+  		flash[:success] = "Welcome to the Social Network"
+  		redirect_to @user
   	else
   		render 'new'
   	end
