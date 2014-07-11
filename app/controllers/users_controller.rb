f27b346bee70c7426feb3252120c00417409fc26
samuelralak <samuelralak@ovi.com>
2014-07-11 14:49:32 +0300

    added correct_user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -57,5 +59,10 @@ class UsersController < ApplicationController
       end
 
       # redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
