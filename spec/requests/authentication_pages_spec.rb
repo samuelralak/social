17426dd09bf1269280c10517609252d6929ca860
samuelralak <samuelralak@ovi.com>
2014-07-11 15:42:38 +0300

    testing that user index action is protected

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -64,7 +64,7 @@ describe "Authentication" do
           end
         end
       end
-      
+
       describe "in the Users controller" do
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
@@ -74,6 +74,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
       end
     end
