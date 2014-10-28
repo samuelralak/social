78b84b4ffb78d65250f8c9778cd44176627ec735
samuelralak <samuelralak@ovi.com>
2014-10-28 21:45:39 +0300

    added wercker.yml

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,8 +9,7 @@ class Micropost < ActiveRecord::Base
   # Returns microposts from the users being followed by the given user
   def self.from_users_followed_by(user)
   	followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-  	where("user_id IN (:followed_user_ids) OR user_id = :user_id", 
-  		followed_user_ids: followed_user_ids, user_id: user)
+  	where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user)
   end
 
 end
